8c2ea64d001440b17fe570de50fdc0ffd232563e
alizarazo <amlizarazos@unal.edu.co>
2022-09-21 17:55:13 -0500

    added level_raw column to bgreadings and reading data in csv file

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/DatabaseUtil.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/DatabaseUtil.java
@@ -231,13 +231,15 @@ public class DatabaseUtil {
                 printStream = new PrintStream(zipOutputStream);
 
                 //add Treatment and BGlucose Header
-                printStream.println("DAY;TIME;UDT_CGMS;GCMS_RAW;BG_LEVEL;CH_GR;BOLUS;REMARK");
+                printStream.println("DAY;TIME;UDT_CGMS;RAW_DATA;RAW_CALCULATED;LEVEL_RAW;BG_LEVEL;CH_GR;BOLUS;REMARK");
 
                 SQLiteDatabase db = Cache.openDatabase();
 
                 // Set all needed Vars
                 double value;
-                double value_raw;
+                double raw_data;
+                double raw_calculated;
+                double level_raw;
                 String valueIE;
                 String valueCHO;
                 String notes;
@@ -250,15 +252,17 @@ public class DatabaseUtil {
                 Date date = new Date();
 
                 //Extract CGMS-Values
-                Cursor cur = db.query("bgreadings", new String[]{"timestamp", "calculated_value", "raw_data"}, "timestamp >= " + from, null, null, null, "timestamp ASC");//KS
+                Cursor cur = db.query("bgreadings", new String[]{"timestamp", "calculated_value", "raw_data", "raw_calculated", "level_raw"}, "timestamp >= " + from, null, null, null, "timestamp ASC");//KS
                 if (cur.moveToFirst()) {
                     do {
                         timestamp = cur.getLong(0);
                         value = cur.getDouble(1);
-                        value_raw = cur.getDouble(2);
+                        raw_data = cur.getDouble(2);
+                        raw_calculated = cur.getDouble(3);
+                        level_raw = cur.getDouble(4);
                         if (value > 13) {
                             date.setTime(timestamp);
-                            printStream.println(df.format(date) + Math.round(value) + ";" + Math.round(value_raw) +";;;;");
+                            printStream.println(df.format(date) + Math.round(value) + ";" + Math.round(raw_data) + ";" + Math.round(raw_calculated) + ";" + Math.round(level_raw) + ";;;;");
                         }
                     } while (cur.moveToNext());
                 }
@@ -271,7 +275,7 @@ public class DatabaseUtil {
                         value = cur.getDouble(1);
                         if (value > 0) {
                             date.setTime(timestamp);
-                            printStream.println(df.format(date) + ";;" + Math.round(value) + ";;;");
+                            printStream.println(df.format(date) + ";;;;" + Math.round(value) + ";;;");
                         }
                     } while (cur.moveToNext());
                 }
@@ -290,7 +294,7 @@ public class DatabaseUtil {
                         notes= notes.replaceAll("\n","||"); //convert linefeed to SiDiary conform expression
                         if (!valueIE.equals("") || !valueCHO.equals("") || !notes.equals("")) {
                             date.setTime(timestamp);
-                            printStream.println(df.format(date) + ";;;" + valueCHO + ";" + valueIE + ";" + notes);
+                            printStream.println(df.format(date) + ";;;;;" + valueCHO + ";" + valueIE + ";" + notes);
                         }
                     } while (cur.moveToNext());
                 }
